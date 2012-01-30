fbe4f9aabc22dc065067c19d958c563641451d95
hachiilcane <hachi.il.cane@gmail.com>
2012-01-31 00:37:44 +0900

    Implemented Basic Authentication.
    - This implementation of authentication is temporary. I would like to use more social user authentication in the near future.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  USERNAME = "owner"
+  PASSWORD = "xxxx"
+
+  before_filter :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |name, password|
+      (@name = name) == USERNAME && password == PASSWORD
+    end
+  end
 end
