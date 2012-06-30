eb389f96ad9a051cfe6af2b091e1804a545cd1df
hachiilcane <hachi.il.cane@gmail.com>
2012-06-30 16:37:18 +0900

    Made the relation between Photo and Basket.

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,2 +1,4 @@
 class Basket < ActiveRecord::Base
+  has_many :pickings
+  has_many :photos, :through => :pickings
 end
--- app/models/picking.rb
+++ app/models/picking.rb
@@ -1,2 +1,4 @@
 class Picking < ActiveRecord::Base
+  belongs_to :photo
+  belongs_to :basket
 end
